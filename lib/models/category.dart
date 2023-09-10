b4e3f20c27fedafd39629fb09c82320e398c3989
Yashraj Kadam <kadamyashraj2004@gmail.com>
2023-09-10 15:25:31 +0530

    displaying Category Items on the Screen

--- lib/models/category.dart
+++ lib/models/category.dart
@@ -5,7 +5,7 @@ class Category {
       {required this.id,
       required this.title,
       this.color = Colors.orange /*setting default colour as Orange*/});
-      
+
   final String id;
   final String title;
   final Color color;
--- lib/screen/categories.dart
+++ lib/screen/categories.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:meals_app/data/dummy_data.dart';
+import 'package:meals_app/widgets/category_grid_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
@@ -10,32 +12,19 @@ class CategoriesScreen extends StatelessWidget {
         title: const Text('Pick you Category'),
       ),
       body: GridView(
+        padding: const EdgeInsets.all(24),
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2, // setting number of columns in my GirdView
-            childAspectRatio: 3 / 2,
-            crossAxisSpacing: 20,
-            mainAxisSpacing: 20),
-        children: const [
-          Text(
-            "1",
-            style: TextStyle(color: Colors.cyanAccent),
-          ),
-          Text(
-            "1",
-            style: TextStyle(color: Colors.cyanAccent),
-          ),
-          Text(
-            "1",
-            style: TextStyle(color: Colors.cyanAccent),
-          ),
-          Text(
-            "1",
-            style: TextStyle(color: Colors.cyanAccent),
-          ),
-          Text(
-            "1",
-            style: TextStyle(color: Colors.cyanAccent),
-          ),
+          crossAxisCount: 2, // setting number of columns in my GirdView
+          childAspectRatio: 1.3,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        children: [
+          // for-in loop is alternative for map function
+          // availableCategories.map((category) => CategoryGridItem(category: category)).toList(),
+          for (final category in availableCategories)
+            // do not use parentheses here when using for-in loop
+            CategoryGridItem(category: category),
         ],
       ),
     );
