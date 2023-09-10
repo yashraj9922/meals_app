0e43f273380c8008d57ff23ae392b42f8ee2dfe1
Yashraj Kadam <kadamyashraj2004@gmail.com>
2023-09-10 14:48:33 +0530

    basic formation of app

---
+++ lib/widgets/category_grid_item.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/models/category.dart';
+
+class CategoryGridItem extends StatelessWidget {
+  const CategoryGridItem({super.key, required this.category});
+  final Category category;
+
+  @override
+  Widget build(BuildContext context) {
+    return ;
+  }
+}
