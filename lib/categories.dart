291cdcd569e2098b1d5c945e727b06d062522428
Yashraj Kadam <kadamyashraj2004@gmail.com>
2023-09-10 12:27:35 +0530

    first commit

---
+++ lib/categories.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Pick you Category'),
+      ),
+      body: GridView(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2, // setting number of columns in my GirdView
+            childAspectRatio: 3 / 2,
+            crossAxisSpacing: 20,
+            mainAxisSpacing: 20),
+        children: const [
+          Text(
+            "1",
+            style: TextStyle(color: Colors.cyanAccent),
+          ),
+          Text(
+            "1",
+            style: TextStyle(color: Colors.cyanAccent),
+          ),
+          Text(
+            "1",
+            style: TextStyle(color: Colors.cyanAccent),
+          ),
+          Text(
+            "1",
+            style: TextStyle(color: Colors.cyanAccent),
+          ),
+          Text(
+            "1",
+            style: TextStyle(color: Colors.cyanAccent),
+          ),
+        ],
+      ),
+    );
+  }
+}
