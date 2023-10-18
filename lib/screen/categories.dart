431975a0cb90cd072c73128175f70f3d68d8fef7
Yashraj Kadam <kadamyashraj2004@gmail.com>
2023-10-18 15:05:06 +0530

    slide up animation

--- lib/screen/categories.dart
+++ lib/screen/categories.dart
@@ -6,6 +6,7 @@ import 'package:meals_app/screen/meals.dart';
 import 'package:meals_app/widgets/category_grid_item.dart';
 
 class CategoriesScreen extends StatefulWidget {
+  // to add explicit animation....class should be in Stateful Widget
   const CategoriesScreen(
       {super.key,
       //// required this.onToggleFavourite,
@@ -20,16 +21,23 @@ class CategoriesScreen extends StatefulWidget {
 
 class _CategoriesScreenState extends State<CategoriesScreen>
     with SingleTickerProviderStateMixin {
-  late AnimationController _animationController;
+  late AnimationController
+      _animationController; // late is used to tell dart that _animationController initially hasno value but later it will be assigned...
+  //there is slightly a timing difference between initState() and build() method...so we need to use late keyword to tell dart that it will be assigned later
   @override
   void initState() {
     super.initState();
     _animationController = AnimationController(
-      vsync: this,
+      vsync:
+          this, // vsync is used to make sure that this animation runs for each fram i.e., 60fps
       duration: const Duration(seconds: 2),
+      lowerBound: 0,
+      upperBound: 1,
       // lowerBound: 0,
       // upperBound: 1,-->default values
     );
+    //starting an animation
+    _animationController.forward();
   }
 
   @override
@@ -67,25 +75,35 @@ class _CategoriesScreenState extends State<CategoriesScreen>
         //   appBar: AppBar(
         //     title: const Text('Pick you Category'),
         //   ),
-        GridView(
-      padding: const EdgeInsets.all(24),
-      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 2, // setting number of columns in my GirdView
-        childAspectRatio: 1.5,
-        crossAxisSpacing: 20,
-        mainAxisSpacing: 20,
-      ),
-      children: [
-        // for-in loop is alternative for map function
-        // availableCategories.map((category) => CategoryGridItem(category: category)).toList(),
-        for (final caTegory in availableCategories)
-          // do not use parentheses here when using for-in loop
-          CategoryGridItem(
-            category: caTegory,
-            onSelectCategory: () => _selectCategory(context, caTegory),
-          ),
-      ],
-      // ),-->Scaffold
-    );
+        // ),-->Scaffold
+
+        AnimatedBuilder(
+            animation: _animationController,
+            child: GridView(
+              padding: const EdgeInsets.all(24),
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2, // setting number of columns in my GirdView
+                childAspectRatio: 1.5,
+                crossAxisSpacing: 20,
+                mainAxisSpacing: 20,
+              ),
+              children: [
+                // for-in loop is alternative for map function
+                // availableCategories.map((category) => CategoryGridItem(category: category)).toList(),
+                for (final caTegory in availableCategories)
+                  // do not use parentheses here when using for-in loop
+                  CategoryGridItem(
+                    category: caTegory,
+                    onSelectCategory: () => _selectCategory(context, caTegory),
+                  ),
+              ],
+            ),
+            builder: (context, child) => Padding(
+                  padding: EdgeInsets.only(
+                      top: 100 - _animationController.value * 100),// slides up animation
+                  // EdgeInsets.only(top: _animationController.value * 100), --> slide down animation
+                  child: child,
+                ));
+    // returning Padding Widget and not directly Girdview will make sure that Gridview widget is not build 60 times but only the padding widget
   }
 }
